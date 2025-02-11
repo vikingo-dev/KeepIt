9732012ac5cee6a37a3ca1deef0f3bab6c5ec52c
Criss Dev <cristian@xiclica.com>
2025-02-10 19:53:58 -0500

    update version to v1.0.5

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const name = import.meta.env.PUBLIC_NAME_APP || "Links" // Nombre de la app, visible en varios lugares
-const version = "v1.0.4"
+const version = "v1.0.5"
 export const configSite = {
   name, version
 }
