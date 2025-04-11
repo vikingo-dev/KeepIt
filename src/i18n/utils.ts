f0347638f54e58caa616c67e6eb844769baa6487
Criss Dev <cristian@xiclica.com>
2025-04-10 21:15:10 -0500

    add function generate token validation for utils

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,8 +1,16 @@
 import { labels } from "./ui";
+import { format } from "date-fns";
+import { configSite } from "config";
 
 const defaultLang = "es"
 export function useTranslations(lang: keyof typeof labels) {
   return function translate(key: keyof typeof labels[typeof defaultLang]) {
     return labels[lang][key] || labels[defaultLang][key]
   }
+}
+
+export function generateTokenAI() {
+  const today = format(new Date(), "yyyy-MM-dd");
+  const tokenContent = `${configSite.name}${today}@vikingo-dev`;
+  return btoa(tokenContent);
 }
